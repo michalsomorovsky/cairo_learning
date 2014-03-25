6f4b4047780502b80cf91e7a5dbfc599bb96396e
michal.somorovsky <m.somorovsky@aliceintokyo.net>
2014-03-25 17:18:05 +0100

    25.32013 ait

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cairo.h>
 #include <gtk/gtk.h>
 #include <math.h>
+#include <pango/pangocairo.h>
 
 static void do_drawing(cairo_t *);
 
@@ -42,15 +43,41 @@ static void do_drawing(cairo_t *cr)
     cairo_arc (cr, xc, yc, radius, angle1, angle2);
     cairo_stroke (cr);
 
-    cairo_set_line_width (cr, 5.0);
+    cairo_set_line_width (cr, 1.0);
     cairo_rectangle(cr, 200.0, 100.0, 100.0, 100.0);
     cairo_stroke(cr);
 
-    cairo_move_to(cr, 0.0, 0.0);
-    cairo_line_to(cr, 100.0, 100.0);
-    cairo_line_to(cr, 100.0, 150.0);
-    cairo_curve_to(cr, 120.0, 130.0, 140.0, 150.0, 200.0, 150.0);
-    cairo_stroke(cr);
+    //cairo_move_to(cr, 0.0, 0.0);
+    //cairo_line_to(cr, 100.0, 100.0);
+    //cairo_line_to(cr, 100.0, 150.0);
+    //cairo_curve_to(cr, 120.0, 130.0, 140.0, 150.0, 200.0, 150.0);
+    //cairo_move_to(cr, 250.0, 150.0);
+    //cairo_text_path(cr, "lama");
+    cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
+    cairo_scale(cr, 1, 0.7);
+    cairo_arc(cr, 300, 100, 50, 0, 2*M_PI);
+    cairo_fill(cr);
+    //cairo_stroke(cr);
+
+    PangoLayout *layout;
+    PangoFontDescription *font_description;
+
+    font_description = pango_font_description_new ();
+    pango_font_description_set_family (font_description, "serif");
+    pango_font_description_set_weight (font_description, PANGO_WEIGHT_BOLD);
+    pango_font_description_set_absolute_size (font_description, 12 * PANGO_SCALE);
+
+    layout = pango_cairo_create_layout (cr);
+    pango_layout_set_font_description (layout, font_description);
+    pango_layout_set_text (layout, "Hello, world", -1);
+
+    cairo_set_source_rgb (cr, 0.0, 0.0, 1.0);
+    cairo_move_to (cr, 250.0, 100.0);
+    pango_cairo_show_layout (cr, layout);
+
+    g_object_unref (layout);
+    pango_font_description_free (font_description);
+
     /* draw helping lines */
     //cairo_set_source_rgba (cr, 1, 0.2, 0.2, 0.6);
     //cairo_set_line_width (cr, 6.0);
@@ -76,7 +103,6 @@ int main(int argc, char *argv[])
 
     darea = gtk_drawing_area_new();
     gtk_container_add(GTK_CONTAINER(window), darea);
-
     g_signal_connect(G_OBJECT(darea), "draw",
                      G_CALLBACK(on_draw_event), NULL);
     g_signal_connect(window, "destroy",
